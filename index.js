5021a06970d3c05c6eb7f7c21ff81d99133ac700
innocent <innonger@gmail.com>
2022-06-16 01:49:13 +0300

    commit tree fc75438f86a08e86d3345caa75ea62381ad2ca74

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+// Test 1 - Send Data
+function submitData(name,email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body:JSON.stringify({
+           name,
+           email
+        } )
+    })
+    // Test 2 - Handle the Response
+    .then(function(resp){
+        return resp.json()
+    })
+    
+    .then(function(object){
+        document.body.innerHTML = object["id"]
+    })
+    // Test 3 - Handle Errors
+    .catch(function(error){
+        document.body.innerHTML = error.message  
+    })
+
+}
+
+
+
+// Test 4 - Return the Fetch Chain
